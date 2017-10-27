a03d743deff132022d8358628dcd4b7289f6f2f9
chhsu_startek <ch@startek-eng.com>
2017-10-27 17:23:26 +0800

    the ObjectID and object destructuring

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,19 @@
-const MongoClient = require('mongodb').MongoClient;
+
+/*
+Object destructuring lets you pull out properties from an object
+creating variables. (ES6 feature)
+
+e.g.
+var user = {name: 'CH', age: 30};
+var {name} = user;
+console.log(name); // CH
+*/
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+// ObjectID constructor function
+
+var obj = new ObjectID();
+console.log(obj);
 
 /*
 In Mongodb, you don't have to create a database before you start using it.
@@ -27,17 +42,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
   // Insert new doc into Users collection (name, age, location)
 
-  db.collection('Users').insertOne({
-    name: 'CH',
-    age: 30,
-    location: 'Taipei'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert Users', err);
-    }
-
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'CH',
+  //   age: 30,
+  //   location: 'Taipei'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert Users', err);
+  //   }
+  //
+  //   //console.log(JSON.stringify(result.ops, undefined, 2));
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
   db.close(); // Close the connection with the MongoDB server.
 });
